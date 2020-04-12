2f59b0567fb9bbe1ebd4bed2bc1e65372662140e
billdelvin <billdelvin6@gmail.com>
2020-04-12 13:06:10 +0700

    fix

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
-const URL = 'https://ghibliapi.herokuapp.com/films'
-axios.get(URL)
-    // .then((res) => {
-    //     return res.json();
-    // })
-    .then((res) => {
-        for (var i = 0; i < 10; i++) {
-            console.log(i);
-        }
-    //    const listFilm = document.getElementById("films")
-       data = res.data;
-    //   console.log(res);}
-    
+const URL = "https://ghibliapi.herokuapp.com/films";
 
-       res.data.forEach(item =>{
-           const {title, director, release_date, rt_score}=item;
-           const itemHTML =`
-           <tr>
-           <td>${title}</td>
-           <td>${director}</td>
-           <td>${release_date}</td>
-           <td>${rt_score}</td>
-           <tr>
-           `;
-        //    listFilm.innerHTML += itemHTML;
+let data = [];
+axios
+	.get(URL)
+	.then((res) => {
+		data = res.data;
+		const listFilm = document.getElementById("films");
+		for (let i = 1; i <= 10; i++) {
+			let no = i;
+			let title = data[i].title;
+			let director = data[i].director;
+			let release_date = data[i].release_date;
+			let rt_score = data[i].rt_score;
+			const itemHTML = `
+                    <tr>
+                    <td>${no}</td>
+                    <td>${title}</td>
+                    <td>${director}</td>
+                    <td>${release_date}</td>
+                    <td>${rt_score}</td>
+                    <tr>
+                `;
+			listFilm.innerHTML += itemHTML;
+		}
+	})
+	.catch((err) => {
+		err;
+	});
 
-        document.getElementById('getData').addEventListener('click',function(event){
-            document.getElementById('films').innerHTML += itemHTML;
-        }
-       )
-    })
-}).catch(err => {
-        console.log(err);
-        
-    })
+// code lama => ini code mu sebelumnya cobak bandingkan 
+
+// axios
+// 	.get(URL)
+// 	// .then((res) => {
+// 	//     return res.json();
+// 	// })
+// 	.then((res) => {
+// 		for (var i = 0; i <= 10; i++) {
+// 			console.log(i);
+// 		}
+// 		//    const listFilm = document.getElementById("films")
+// 		data = res.data;
+// 		//   console.log(res);}
+
+// 		res.data.forEach((item) => {
+// 			const { title, director, release_date, rt_score } = item;
+// 			const itemHTML = `
+//            <tr>
+//            <td>${title}</td>
+//            <td>${director}</td>
+//            <td>${release_date}</td>
+//            <td>${rt_score}</td>
+//            <tr>
+//            `;
+// 			//    listFilm.innerHTML += itemHTML;
+
+// 			document
+// 				.getElementById("getData")
+// 				.addEventListener("click", function (event) {
+// 					document.getElementById("films").innerHTML += itemHTML;
+// 				});
+// 		});
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
